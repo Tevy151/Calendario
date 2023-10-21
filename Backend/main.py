ca8946ef2e281bc75007fbe0ea3a7de1f71112ef
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-10-21 00:06:21 -0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,4 @@
+from urllib import response
 from tables import createTables
 import time
 import psycopg2
@@ -5,6 +6,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import List
 
 SECRET_KEY = "RANDOM"
 ALGORITHM = "HS256"
@@ -46,8 +48,25 @@ class Evaluacion(BaseModel):
 @app.post("/AddEvaluacion/", response_model=Evaluacion)
 async def updateEvaluaciones(event: Evaluacion):
     try:
-        cur.execute("INSERT INTO evaluaciones (id, asignatura, descrip, tipo, dia) VALUES (%s, %s, %s, %s, %s);", (event.id, event.asignatura, event.description, event.tipo, event.dia))
+        print("INSERT INTO evaluaciones (id, asignatura, descrip, tipo, dia) VALUES ("+event.id+",\'"+event.asignatura+"\',\'"+event.description+"\',\'"+event.tipo+"\',"+event.dia+");")
+        cur.execute("INSERT INTO evaluaciones (id, asignatura, descrip, tipo, dia) VALUES ("+event.id+",\'"+event.asignatura+"\',\'"+event.description+"\',\'"+event.tipo+"\',"+event.dia+");")
         conn.commit()
         return JSONResponse(content={"message": "Evaluación agregada"}, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al agregar la evaluación: {str(e)}")
+
+@app.get("/Eventos/", response_model=List[Evaluacion])
+def get_eventos():
+    cur.execute("SELECT * FROM evaluaciones ORDER BY id ASC")
+    rows = cur.fetchall()
+    result = []
+    for row in rows:
+        eval_dict = {
+            "id": row[0],
+            "asignatura": row[1],
+            "description": row[2],
+            "tipo": row[3],
+            "dia": row[4]
+        }
+        result.append(eval_dict)
+    return result
