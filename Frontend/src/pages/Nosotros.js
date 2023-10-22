be3a9a10e9daa0a2384862c8caffe340a458be48
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-10-22 18:27:18 -0300

    Create Nosotros.js

---
+++ Frontend/src/pages/Nosotros.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import NavBar from './Navbar.js';
+
+export const About = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container">
+        
+        <div class="contenedor-imagen">
+      
+        </div>
+        <div className="container">
+          
+          <h3>Nosotros</h3>
+
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default About;
