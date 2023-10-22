e9688ebcfa8c0f69c817f5b663c9647e738dde2a
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-10-22 18:26:03 -0300

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Calendario from './pages/Calendario';
+import Nosotros from './pages/Nosotros'
+//
 function App() {
   return (
     <Router>
@@ -8,6 +10,8 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/Home" element={<Home/>} />
+        <Route path="/Calendario" element={<Calendario/>} />
+        <Route path="/Nosotros" element={<Nosotros/>} />
         </Routes>
       </div>
     </Router>
