c3ea9b84d92ef4c877ab3ff3ed2dac8f00e7370b
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-12-02 20:08:50 -0300

    Update Home.js

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -10,13 +10,13 @@ export const HomePage = () => {
       <div className="container">
         <link rel="stylesheet" type="text/css" href={style}></link>
         
-        <div class="contenedor-imagen">
+        <div className="contenedor-imagen">
           <img src={USM} alt="Descripción de la imagen" height='250px'></img>
         </div>
 
         <div className="container">
           <h3>¡Bienvenido al calendario USM!</h3>
-          <div>Para comenzar dirigite a la pestaña Calendario</div>
+          <div>Para agregar evaluaciones dirigirse a la pestaña Calendario</div>
         </div>
 
       </div>
