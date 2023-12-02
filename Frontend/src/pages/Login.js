885538d6bd637ab010683fdb8330148ceac4e6cf
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-12-02 20:04:15 -0300

    Update Login.js

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import estilo from "../styles/style.css";
 import NavBar from './Navbar.js';
 import '../styles/Login.css'; // Importamos el archivo de estilos CSS
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 const API = "http://localhost:3001"
 
@@ -49,41 +59,83 @@ const Formulario = () => {
     }
   };
 
+  const defaultTheme = createTheme();
+
   return (
     <div>
+    <link href={estilo} rel="stylesheet" />
     <NavBar/>
-    <div class="container">
-      <link href={estilo} rel="stylesheet" />
-      
-    <form onSubmit={handleSubmit}>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+      <Box component="form" onSubmit={handleSubmit}  noValidate sx={{ mt: 1 }}>
       <br />
       <label>
         E-Mail:
-        <input
-          type="email"
-          value={correo}
-          onChange={(e) => setcorreo(e.target.value)}
-        />
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              type="email"
+              value={correo}
+              onChange={(e) => setcorreo(e.target.value)}
+            />
+        <br></br>
+        Contraseña
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+        <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+            Enviar
+        </Button>
+        {mostrarMensaje && <p className="red-text">Credenciales incorrectas </p>}
+        <Grid container>
+          <Grid item>
+            <Button
+                href="/register"
+              >
+              No tienes cuenta? Registrate
+          </Button>
+          </Grid>
+        </Grid>
       </label>
-      <br />
-      <label>
-        Contraseña:
-        <input
-          type="String"
-          value={password}
-          onChange={(e) => setpassword(e.target.value)}
-        />
-      </label>
-      <button type="submit">Enviar</button>
-      {mostrarMensaje && <p>Credenciales incorrectas </p>}
-      <Link to="/">
-          <button type="button">Volver</button>
-      </Link>
-      <Link to="/register">
-          <button type="button">Registrar</button>
-      </Link>
-    </form>
-    </div>
+      </Box>
+      </Box>
+    </Container>
+    </ThemeProvider>
     </div>
   );
 };
