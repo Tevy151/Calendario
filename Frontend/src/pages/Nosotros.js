b78ee3aaf1ae6b1cc449d14a07f38d850d6c9e14
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-12-02 20:09:19 -0300

    Update Nosotros.js

--- Frontend/src/pages/Nosotros.js
+++ Frontend/src/pages/Nosotros.js
@@ -10,13 +10,21 @@ export const About = () => {
       <div className="container">
         <link rel="stylesheet" type="text/css" href={style}></link>
         
-        <div class="contenedor-imagen">
+        <div className="contenedor-imagen">
           <img src={LOGO} alt="Descripción de la imagen" height='250px'></img>
         </div>
 
         <div className="container">
           <h3>SuChoripan</h3>
-          <div>Hola somos SuChoripan</div>
+          <p>Hola somos SuChoripan, un grupo de alumnos de la USM. Somos los responsables detras de esta aplicación como parte de un proyecto de la asignatura Ingeniería, Informatica y Sociedad</p>
+          <br></br>
+          <h3>Integrantes:</h3>
+          <p>José Campos</p>
+          <p>Sebastián Sandoval</p>
+          <p>Vicente Mackenzie</p>
+          <p>Cristobal Guerrero</p>
+          <p>Felipe González</p>
+          <p>Francisco Muñoz</p>
         </div>
 
       </div>
