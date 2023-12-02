b6aafd6f7a558b6e4f8ad06d641f3858af87e527
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-12-02 20:03:58 -0300

    Update Registro.js

--- Frontend/src/pages/Registro.js
+++ Frontend/src/pages/Registro.js
@@ -4,6 +4,15 @@ import axios from 'axios';
 import estilo from "../styles/style.css";
 import NavBar from './Navbar.js';
 import '../styles/Login.css'; // Importamos el archivo de estilos CSS
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const API = "http://localhost:3001"
 
@@ -51,52 +60,92 @@ const Formulario = () => {
     }
   };
 
+  const defaultTheme = createTheme();
+
   return (
     <div>
+    <link href={estilo} rel="stylesheet" />
     <NavBar/>
-    <div class="container">
-      <link href={estilo} rel="stylesheet" />
-      
-    <form onSubmit={handleSubmit}>
-      <br />
-      <label>
-        E-Mail:
-        <input
-          type="email"
-          value={correo}
-          onChange={(e) => setcorreo(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Contraseña:
-        <input
-          type="String"
-          value={password}
-          onChange={(e) => setpassword(e.target.value)}
-        />
-      </label>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Registro
+          </Typography>
+      <Box component="form" onSubmit={handleSubmit}  noValidate sx={{ mt: 1 }}>
       <br />
       <label>
         Nombre:
-        <input
-          type="String"
-          value={nombre}
-          onChange={(e) => setnombre(e.target.value)}
-        />
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="nombre"
+              label="Nombre"
+              name="nombre"
+              type="String"
+              value={nombre}
+              onChange={(e) => setnombre(e.target.value)}
+            />
+        <br></br>    
+        E-Mail:
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo"
+              name="email"
+              type="email"
+              value={correo}
+              onChange={(e) => setcorreo(e.target.value)}
+            />  
+        <br></br>
+        Contraseña
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+        {mostrarMensaje && <p className="green-text">Usuario creado exitosamente</p>}
+        <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+            Enviar
+        </Button>
+        
+        <Grid container>
+          <Grid item>
+            <Button
+                href="/login"
+              >
+              Ya tienes cuenta? Inicia sessión
+          </Button>
+          </Grid>
+        </Grid>
       </label>
-      <br />
-      {mostrarMensaje && <p>Usuario creado exitosamente </p>}
-      <button type="submit">Enviar</button>
-
-      <Link to="/">
-          <button type="button">Volver</button>
-      </Link>
-      <Link to="/login">
-          <button type="button">Login</button>
-      </Link>
-    </form>
-    </div>
+      </Box>
+      </Box>
+    </Container>
+    </ThemeProvider>
     </div>
   );
 };
